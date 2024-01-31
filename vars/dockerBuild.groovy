6c3cc20167782eb840b1ddf951672d49712b66d4
poorna162 <126664341+poorna162@users.noreply.github.com>
2024-01-31 18:20:38 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,6 +1,6 @@
 def call(String dockerHubUsername, String imageName) {
     // Build the Docker image
-    sh "docker build --build-arg REACT_APP_RAPID_API_KEY=d6cfeb2ee5msh55afdf92394b7c2p1f4da7jsn81f6bc09e244 -t ${imageName} ."
+    sh "docker build --build-arg REACT_APP_RAPID_API_KEY=sqa_197fb22af06d2747c0cf2174aeb0bd98eab7af73 -t ${imageName} ."
      // Tag the Docker image
     sh "docker tag ${imageName} ${dockerHubUsername}/${imageName}:latest"
     // Push the Docker image
